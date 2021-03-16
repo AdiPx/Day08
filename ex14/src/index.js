a215de468fa9c594361d83755c4ed8a438cd588d
Adipx <adipasic86@gmail.com>
2021-03-16 10:50:09 +0100

    .

---
+++ ex14/src/index.js
@@ -0,0 +1,8 @@
+// Only change code below this line
+function convertStrToInt(str){
+   var srt = parseInt(srt);
+   
+}
+convertStrToInt(06);
+// Only change code above this line  
+module.exports = convertStrToInt;
